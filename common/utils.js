5a07bd3891ed7771e643798f834c7c060810bd60
Max Damewood <maxdamewood@gmail.com>
2023-04-29 12:36:01 -0400

    Added utils.js for percent format & group by funcs

--- common/utils.js
+++ common/utils.js
@@ -1,16 +1,44 @@
+// Define an empty object named "utils"
 const utils = {};
 
+// Define a method "formatPercent" on the "utils" object that takes a number "n" as input and returns the percentage value with two decimal places
 utils.formatPercent = (n) => {
   return (n * 100).toFixed(2) + "%";
 };
 
+// Define a method "printProgress" on the "utils" object that takes two numbers "count" and "max" as input and prints the progress on the console.
+// The progress is displayed in the format "count/max (percentage)"
 utils.printProgress = (count, max) => {
+  // Clear the current line on the console
   process.stdout.clearLine();
+  // Move the cursor to the beginning of the line on the console
   process.stdout.cursorTo(0);
+  // Calculate the percentage value of the progress
   const percent = utils.formatPercent(count / max);
+  // Write the progress on the console in the format "count/max (percentage)"
   process.stdout.write(count + "/" + max + " (" + percent + ")");
 };
 
+// This function groups an array of objects by a specified key
+utils.groupBy = (objArray, key) => {
+  // Create an empty object to hold the groups
+  const groups = {};
+  // Iterate over each object in the input array
+  for (let obj of objArray) {
+    // Get the value of the specified key for the current object
+    const val = obj[key];
+    // If the value doesn't exist in the groups object, create an empty array for it
+    if (groups[val] == null) {
+      groups[val] = [];
+    }
+    // Add the current object to the array for the corresponding value in the groups object
+    groups[val].push(obj);
+  }
+  // Return the object of grouped arrays
+  return groups;
+};
+
+// If the "module" object exists (i.e., the code is running in a Node.js environment), export the "utils" object
 if (typeof module !== "undefined") {
   module.exports = utils;
 }
